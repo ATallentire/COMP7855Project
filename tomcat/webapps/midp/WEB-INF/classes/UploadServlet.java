864628ffed86b9916c1f90dd4297478b31c3e7c3
GHowardEng <60199078+GHowardEng@users.noreply.github.com>
2020-03-23 18:37:57 -0700

    Updated upload page

--- tomcat/webapps/midp/WEB-INF/classes/UploadServlet.java
+++ tomcat/webapps/midp/WEB-INF/classes/UploadServlet.java
@@ -24,6 +24,12 @@ public class UploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private ServletFileUpload uploader = null;
 	private String imDir = "C:/COMP7855Project/tomcat/webapps/midp/Images";
+	String date = "";
+	String caption = "";
+	String latitude = "";
+	String longitude = "";
+	String fileName = "";
+	
 	@Override
 	public void init() throws ServletException{
 		DiskFileItemFactory fileFactory = new DiskFileItemFactory();
@@ -61,36 +67,76 @@ public class UploadServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(!ServletFileUpload.isMultipartContent(request)){
+		/*if(!ServletFileUpload.isMultipartContent(request)){
 			throw new ServletException("Content type is not multipart/form-data");
-		}
+		}*/
 		
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.write("<html><head></head><body>");
+		
+
 		try {
 			List<FileItem> fileItemsList = uploader.parseRequest(request);
 			Iterator<FileItem> fileItemsIterator = fileItemsList.iterator();
+			System.out.println(request.getParameter("source"));
+
 			while(fileItemsIterator.hasNext()){
 				FileItem fileItem = fileItemsIterator.next();
+				
 				System.out.println("FieldName="+fileItem.getFieldName());
 				System.out.println("FileName="+fileItem.getName());
 				System.out.println("ContentType="+fileItem.getContentType());
 				System.out.println("Size in bytes="+fileItem.getSize());
 				
+				if( fileItem.isFormField()){
+					System.out.println("FormField");
+					
+					String fieldname = fileItem.getFieldName();
+					String fieldvalue = fileItem.getString();
+					System.out.println(fieldname+"\n " +fieldvalue);
+					if(fieldname.equals("date")){
+						date = fieldvalue;
+					}
+					else if(fieldname.equals("caption")){
+						caption = fieldvalue;
+					}
+					else if(fieldname.equals("lat")){
+						latitude = fieldvalue;
+					}
+					else if(fieldname.equals("long")){
+						longitude = fieldvalue;
+					}
+				}
+
+				else{
 				File file = new File(imDir+File.separator+fileItem.getName());
 				System.out.println("Absolute Path at server="+file.getAbsolutePath());
 				fileItem.write(file);
-				out.write("File "+fileItem.getName()+ " uploaded successfully.");
-				out.println(
-						"<html>\n" +
-							"<br />\n" +
-							"<br />\n" +	
-							"<input type=\"button\" value=\"Back to Search\" onclick=\"location.href='http://localhost:8081/midp/hits';\" />\n" +
-							"</div>\n</form>\n" +
-							"</form>\n</body>\n</html>");
-				//out.write("<a href=\"UploadDownloadFileServlet?fileName="+fileItem.getName()+"\">Download "+fileItem.getName()+"</a>");
+				fileName = fileItem.getName();
+				
+				}
 			}
+			out.write("File "+fileName+ " uploaded successfully.");
+			out.write(
+			"<html>\n" +
+			"<br />\n" +
+			"<br />\n" +
+			"  <li><b>Date</b>: "
+			+ date + "\n" +
+			"  <li><b>Caption</b>: "
+			+ caption + "\n" +	
+			"  <li><b>Latitude</b>: "
+			+ latitude + "\n" +
+			"  <li><b>Longitude</b>: "
+			+ longitude+ "\n" +
+			"<br />\n" +
+			"<br />\n" +
+			"<input type=\"button\" value=\"Back to Search\" onclick=\"location.href='http://localhost:8081/midp/hits';\" />\n" +
+			"</div>\n</form>\n" +
+			"</form>\n</body>\n</html>");
+
+			
 		} catch (FileUploadException e) {
 			out.write("Exception in uploading file.");
 		} catch (Exception e) {
